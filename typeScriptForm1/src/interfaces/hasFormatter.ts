ca8d8f8b060a8a334e8242b5dcbc97ca23bec1c2
Emilianalila <emilianalila@gmail.com>
2024-03-06 09:51:58 +0100

    refactor interface

--- typeScriptForm1/src/interfaces/hasFormatter.ts
+++ typeScriptForm1/src/interfaces/hasFormatter.ts
@@ -1,3 +1,5 @@
-export interface HasFormatter { // implements is used when you want to make sure that a class has certain properties and methods defined by an interface,
+export interface HasFormatter { // implements is used when you want to make sure that a class has certain properties and methods defined by an interface, is because we use implements in the classes, we say: class Invoice implements HasFormatter (nos aseguramos que las clases q yo tengo tengan si o si tal metodo)
   format():string; // this function has to return a string
 }
+
+/* Si una clase implementa una interfaz pero no proporciona un método "format()" que coincida con la firma requerida por la interfaz, o si el método proporcionado no devuelve el tipo especificado osea un "string", se generará un error en tiempo de compilación. */
